2428b4195687a690824906857464c808e1f9ed35
bigdev14bit <devbig14@gmail.com>
2026-04-22 02:50:49 +0100

    Added new files

---
+++ assignment/tearsofjoy/PositiveNum.py
@@ -0,0 +1,7 @@
+numberOne = 11
+numberTwo = 19
+
+if numberOne > numberTwo:
+   print(numberOne - numberTwo)
+else:
+   print(numberTwo - numberOne)
---
+++ assignment/tearsofjoy/PositiveNumberPython.py
@@ -0,0 +1,6 @@
+def positiveNumber(numberOne, numberTwo):
+  if numberOne > numberTwo:
+    return numberOne - numberTwo
+  else:
+    return numberTwo - numberOne
+print(positiveNumber(11, 19))
